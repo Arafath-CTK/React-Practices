4180daaefb4afb1434afa36907e9735ead35154c
Arafath <arafathctk@gmail.com>
2024-05-22 12:58:10 +0530

    daily practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+// function App() {
+//   const items = ["Starboy", "Baby", "Dandelions", "Levitating", "Blinding Lights", "Shape of You"];
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+
+//   return (
+//     <div>
+//       <ListGroup items={items} heading="Songs" onSelectItem={handleSelectItem} />
+//     </div>
+//   );
+// }
+
+// import Alert from "./components/Alert";
+// function App() {
+//   return (
+//     <div>
+//       <Alert>
+//         Hello <span>World</span>
+//       </Alert>
+//     </div>
+//   );
+// }
+
+// import Button from "./components/Button";
+// function App() {
+//   return (
+//     <div>
+//       <Button onClick={() => console.log("Clicked")}>Click here</Button>
+//     </div>
+//   );
+// }
+
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
-  const items = ["Starboy", "Baby", "Dandelions", "Levitating", "Blinding Lights", "Shape of You"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  const [display, setDisplay] = useState(false);
   return (
     <div>
-      <ListGroup items={items} heading="Songs" onSelectItem={handleSelectItem} />
+      {display && (
+        <Alert onClose={() => setDisplay(false)}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      <Button onClick={() => setDisplay(true)}>Click here</Button>
     </div>
   );
 }
--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,7 +1,21 @@
-const Alert = () => {
-  return (
-    <div>Alert</div>
-  )
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
 }
 
-export default Alert
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
